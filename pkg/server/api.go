e6e2de3307b08b0f007d4612416d87a5b21ca908
Andrés Felipe Martínez Salazar <andresfelipe.2031@gmail.com>
2020-09-15 20:15:16 -0500

    Paths are case insensitive and add final slash

--- pkg/server/api.go
+++ pkg/server/api.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -23,16 +26,32 @@ func New(client *mongo.Client) Server {
 	a := &Api{}
 	r := mux.NewRouter()
 	// r.HandleFunc("/", a.fetchGopher).Methods(http.MethodGet)
-	r.HandleFunc("/types", a.FetchTypes).Methods(http.MethodGet)
-	r.HandleFunc("/states", a.FetchStates).Methods(http.MethodGet)
-	r.HandleFunc("/genres", a.FetchGenres).Methods(http.MethodGet)
-	r.HandleFunc("/animes", a.FetchAnimes).Methods(http.MethodGet)
-	r.HandleFunc("/latest", a.FetchLatestEpisodes).Methods(http.MethodGet)
+	r.Use(func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Printf("%s %s %s\n", r.URL, r.Method, time.Now().Format("2006-01-02 15:04:05"))
+			h.ServeHTTP(w, r)
+		})
+	})
+	r.HandleFunc("/types/", a.FetchTypes).Methods(http.MethodGet)
+	r.HandleFunc("/states/", a.FetchStates).Methods(http.MethodGet)
+	r.HandleFunc("/genres/", a.FetchGenres).Methods(http.MethodGet)
+	r.HandleFunc("/animes/", a.FetchAnimes).Methods(http.MethodGet)
+	r.HandleFunc("/latest/", a.FetchLatestEpisodes).Methods(http.MethodGet)
 	a.router = r
 	return a
 }
 
-// Router reutnr the api router
+// Router return the api router
 func (a *Api) Router() http.Handler {
-	return a.router
+	return CaselessMatcher(a.router)
+}
+
+func CaselessMatcher(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.URL.Path = strings.ToLower(r.URL.Path)
+		if !strings.HasSuffix(r.URL.Path, "/") {
+			r.URL.Path = r.URL.Path + "/"
+		}
+		next.ServeHTTP(w, r)
+	})
 }
