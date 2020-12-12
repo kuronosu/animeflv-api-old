fa25492b60d412ac6ec059f881a2ef57b930d4c8
Andrés Felipe Martínez Salazar <andresfelipe.2031@gmail.com>
2020-12-12 18:27:56 -0500

    More colors

--- pkg/utils/color.go
+++ pkg/utils/color.go
@@ -2,61 +2,85 @@ package utils
 
 import (
 	"log"
-	"strings"
 )
 
-const colorReset = "\033[0m"
-const colorRed = "\033[31m"
-const colorGreen = "\033[32m"
-const colorYellow = "\033[33m"
-const colorBlue = "\033[34m"
-const colorPurple = "\033[35m"
-const colorCyan = "\033[36m"
-const colorWhite = "\033[37m"
+// NC No color
+const NC = "\033[0m"
 
-// ColoredText return text with the specified color
+// Black color
+const Black = "\033[0;30m"
+
+// Red color
+const Red = "\033[0;31m"
+
+// Green color
+const Green = "\033[0;32m"
+
+// BrownOrange color
+const BrownOrange = "\033[0;33m"
+
+// Blue color
+const Blue = "\033[0;34m"
+
+// Purple color
+const Purple = "\033[0;35m"
+
+// Cyan color
+const Cyan = "\033[0;36m"
+
+// LightGray color
+const LightGray = "\033[0;37m"
+
+// DarkGray color
+const DarkGray = "\033[1;30m"
+
+// LightRed color
+const LightRed = "\033[1;31m"
+
+// LightGreen color
+const LightGreen = "\033[1;32m"
+
+// Yellow color
+const Yellow = "\033[1;33m"
+
+// LightBlue color
+const LightBlue = "\033[1;34m"
+
+// LightPurple color
+const LightPurple = "\033[1;35m"
+
+// LightCyan color
+const LightCyan = "\033[1;36m"
+
+// White color
+const White = "\033[1;37m"
+
+// ColoredText return text with the specified Color
 func ColoredText(text string, color string) string {
-	switch strings.ToLower(color) {
-	case "red":
-		return string(colorRed) + text + string(colorReset)
-	case "green":
-		return string(colorGreen) + text + string(colorReset)
-	case "yellow":
-		return string(colorYellow) + text + string(colorReset)
-	case "blue":
-		return string(colorBlue) + text + string(colorReset)
-	case "purple":
-		return string(colorPurple) + text + string(colorReset)
-	case "cyan":
-		return string(colorCyan) + text + string(colorReset)
-	case "white":
-		return string(colorWhite) + text + string(colorReset)
-	default:
-		return text
-	}
+	return string(color) + text + string(NC)
 }
 
 // InfoLog print log in blue color
 func InfoLog(text string) {
-	log.Println(ColoredText(text, "blue"))
+	log.Println(ColoredText(text, LightBlue))
 }
 
 // SuccessLog print log in blue color
 func SuccessLog(text string) {
-	log.Println(ColoredText(text, "green"))
+	log.Println(ColoredText(text, LightGreen))
 }
 
 // WarningLog print log in yellow color
 func WarningLog(text string) {
-	log.Println(ColoredText(text, "yellow"))
+	log.Println(ColoredText(text, Yellow))
 }
 
 // ErrorLog print log in red color
 func ErrorLog(text string) {
-	log.Println(ColoredText(text, "red"))
+	log.Println(ColoredText(text, LightRed))
 }
 
 // FatalLog call log.Fatal with red color
 func FatalLog(text string) {
-	log.Fatal(ColoredText(text, "red"))
+	log.Println(ColoredText(text, Red))
 }
